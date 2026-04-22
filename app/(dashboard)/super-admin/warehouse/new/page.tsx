c27dab75d23518aeb459337544891f3bab0b989a
djuraku1ov <sarik.j2445@gmail.com>
2026-04-22 22:43:53 +0500

    feat: add /super-admin/warehouse/new wizard page

---
+++ app/(dashboard)/super-admin/warehouse/new/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import WarehouseSetupWizard from "@/components/super-admin/warehouse/wizard/WarehouseSetupWizard";
+
+export default function NewWarehousePage() {
+    return <WarehouseSetupWizard />;
+}
